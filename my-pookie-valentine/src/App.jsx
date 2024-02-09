aef117063449941708c308f405fd27f5ec560556
Olga Tomashava <78414186+olyahelga@users.noreply.github.com>
2024-02-09 18:48:13 +0000

    Implement buttons functionality.

--- my-pookie-valentine/src/App.jsx
+++ my-pookie-valentine/src/App.jsx
@@ -1,16 +1,35 @@
 import bear from './love-bear.gif';
+import helga from './helga.jpeg';
 import './App.scss';
+import { useState } from 'react';
+
 
 function App() {
+  const [helgaVisible, setHelgaVisible] = useState(false);
+  const [position, setPosition] = useState({ x: null, y: null });
+
+  const handleHover = () => {
+    const newX = Math.random() * (window.innerWidth - 100); // Adjust 100 to your button's width
+    const newY = Math.random() * (window.innerHeight - 100); // Adjust 100 to your button's height
+    setPosition({ x: newX, y: newY });
+  };
   return (
     <div className="App">
       <header className="App-header">
-        <h2>will u be my валентинка?</h2>
-        <img src={bear} />
-        <div className="buttonsContainer">
-          <button>Yes</button>
-          <button>No</button>
-        </div>
+        <span style={{ display: !helgaVisible ? 'block' : 'none' }}>
+          <h2>will u be my валентинка?</h2>
+          <img src={bear} />
+          <div className="buttonsContainer">
+            <button onClick={() => setHelgaVisible(true)}>Yes</button>
+            <button style={position.x !== null ? {
+              position: 'absolute',
+              top: position.y + 'px',
+              left: position.x + 'px',
+            } : {}}
+              onMouseEnter={handleHover} >No</button>
+          </div>
+        </span>
+        <img style={{ display: helgaVisible ? 'block' : 'none' }} className='helgaImg' src={helga} />
       </header>
     </div>
   );
